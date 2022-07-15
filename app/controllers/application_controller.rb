3471e5c03f4af150cad1ffeab0ef0ae4a56717f7
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-07-15 12:01:12 +0000

    [Fix]login後のリンクをマイページに修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    about_path
+    user_path(resource.id) #ここの"about"部分を変更？
   end
 
   def after_sign_out_path_for(resource)
