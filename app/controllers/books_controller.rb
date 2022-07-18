ee0100ef413f19adadd8669e3d96f5ba3f18e0f3
EC2 Default User <ec2-user@ip-172-31-8-2.ap-northeast-1.compute.internal>
2022-07-18 09:55:59 +0000

    [Add]userの編集機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to "/books/#{@book.id}"
+  end
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to "/users/#{@user.id}"
   end
 
   def index
@@ -16,6 +19,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+    private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
 
 end
 
